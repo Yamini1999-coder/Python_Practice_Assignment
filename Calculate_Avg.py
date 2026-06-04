e11d5bbf72fd304ab1b18119e7853b6f1d1b1ae0
Yamini1999-coder <yamini3959@gmail.com>
2026-06-04 13:34:32 +0530

    Delete Calculate_Avg.py

--- Calculate_Avg.py
+++
@@ -1,5 +0,0 @@
-# Take an input of 2 floating no.s from user and perform their avg
-a= float(input("Enter 1st no.:"))
-b= float(input("Enter the 2nd no.:"))
-AVG = a+b/2
-print ("Average of given two numbers is:", AVG)
