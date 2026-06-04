f0fe4eb127e3bccc1cf9d6bce6f650bd105cb825
Yamini1999-coder <yamini3959@gmail.com>
2026-06-04 13:34:53 +0530

    Delete sum-1.py

--- sum-1.py
+++
@@ -1,5 +0,0 @@
-# write a prog to take an i/p from user and perform their sum
-a=int(input("Enter first no.:"))
-b=int(input("Enter 2nd No.:" ))
-SUM= a+b
-print ("Sum of given two numbers is:", SUM)
